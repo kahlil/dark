bb6e60cdb607da7779fcdb29f233b2d174574c98
Kahlil Lechelt <hello@kahlil.info>
2020-12-28 16:22:45 +0100

    add Chakra provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default App
