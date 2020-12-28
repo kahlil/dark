c58e7e32c23a15c06301d921c22ebd9ecfd8b310
Kahlil Lechelt <hello@kahlil.info>
2020-12-28 16:38:36 +0100

    add initial color mode to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import { ColorModeScript } from '@chakra-ui/react'
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
   render() {
     return (
       <Html>
         <Head />
         <body>
+          <ColorModeScript initialColorMode="dark" />
           <Main />
           <NextScript />
         </body>
