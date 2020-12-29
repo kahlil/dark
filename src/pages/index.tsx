11d54f5f9a77bc5a635a4a284bbe297b3ae3991a
Kahlil Lechelt <hello@kahlil.info>
2020-12-29 16:30:39 +0100

    add copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
-import { Container, Heading, VStack } from '@chakra-ui/react'
+import { Container, Heading, VStack, Text, Code } from '@chakra-ui/react'
 
 function Home() {
   return (
     <Container>
-      <VStack>
-        <Heading as="h1" fontSize="9xl">
-          Grit
+      <VStack my={24} spacing={12}>
+        <Heading as="h1" fontSize="9xl" textAlign="center">
+          Dark
         </Heading>
+        <Text fontSize="3xl" textAlign="center">
+          An opinated, dark-themed Next.js &amp; Chakra UI GitHub repository
+          template
+        </Text>
+        <Text>
+          Hit the ground running with one <Code>yarn install</Code>.
+        </Text>
       </VStack>
     </Container>
   )
