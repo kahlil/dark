58859ba00c793e391d4c39b08e9ec96813582fda
Kahlil Lechelt <hello@kahlil.info>
2020-12-29 14:18:47 +0100

    extract custom theme

---
+++ src/chakra-ui/customTheme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+const customTheme = extendTheme({ config });
+
+export default customTheme;
